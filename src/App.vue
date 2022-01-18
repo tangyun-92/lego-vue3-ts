4f77f6f588c65e5564e2900c9f94e7b8f541957c
tangyun <tangyun1992@qq.com>
2022-01-18 17:32:29 +0800

    完成项目路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <template>
   <div class="homepage-container">
-    <a-layout :style="{ background: '#fff' }">
-      <a-layout-header class="header">
-        <div class="page-title">
-          <router-link to="/">慕课乐高</router-link>
-        </div>
-      </a-layout-header>
-      <a-layout-content class="home-layout">
-        <router-view></router-view>
-      </a-layout-content>
-      <a-layout-footer>
-        © 哈哈哈 (hahaha.com) 版权所有 | 川TCP备22288888号
-      </a-layout-footer>
-    </a-layout>
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-// import Home from './views/home.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    // Home
-  }
+  name: 'App'
 })
 </script>
 
 <style>
-.page-title {
-  color: #fff;
-}
-.content-container {
-  background: #fff;
-  padding: 0 24px 24px 30px;
-  min-height: 85vh;
-  max-width: 1200px;
-  margin: 50px auto;
-  width: 100%;
-}
 </style>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,24 +2,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/home.vue'
 import Editor from '../views/editor.vue'
 import TemplateDetail from '../views/templateDetail.vue'
+import Index from '../views/index.vue'
 
+/**
+ * 
+ */
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'index',
+      component: Index,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: Home,
+        },
+        {
+          path: 'template/:id',
+          name: 'template',
+          component: TemplateDetail
+        },
+      ],
     },
     {
       path: '/editor',
       name: 'editor',
       component: Editor,
-    },
-    {
-      path: '/template/:id',
-      name: 'template',
-      component: TemplateDetail,
     },
   ],
 })
